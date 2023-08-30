177b837b98bef0135ad47c31b6daf583f5970ad3
sage319 <sage319@netmarble.com>
2023-08-30 18:48:49 +0900

    Coins 스타일 적용

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from "styled-components";
 export const lightTheme : DefaultTheme = {
     bgColor: "#2f3640",
     textColor: "#7f8fa6",
-    accentColor: "#00a8ff",
+    accentColor: "#9c88ff",
 };
