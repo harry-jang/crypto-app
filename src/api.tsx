34b1cd996c4330bc3b756cea060bdc7323d19ed9
Harry Jang <harry.jang@kakao.com>
2023-08-31 23:43:04 +0900

    차트 데이터 가져오기

--- src/api.tsx
+++ src/api.tsx
@@ -11,3 +11,10 @@ export function fetchCoinInfo(coinId:string) {
 export function fetchCoinTickers(coinId:string) {
     return fetch(`${BASE_URL}/tickers/${coinId}`).then(response => response.json());
 }
+
+export function fetchCoinHistory(coinId:string) {
+    // nico API에서는 미사용
+    //const endDate = Math.floor(Date.now() / 1000);
+    //const startDate = endDate - 60 * 60 * 24 * 7;
+    return fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`).then(response => response.json());
+}
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,14 @@
-function Chart() {
-    return <h1>Chart</h1>;
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+
+interface CharProps {
+    coinId : string;
+}
+
+function Chart({coinId} : CharProps) {
+    const {isLoading, data : historyData} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+    return <h1>{coinId}</h1>;
 }
 
 export default Chart;
